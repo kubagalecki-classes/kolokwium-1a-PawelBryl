351ffcfe2baec331e91391aeb787419a9282d3fa
PawelBryl <73634622+PawelBryl@users.noreply.github.com>
2021-01-14 13:24:26 +0100

    Update zad2.hpp

--- include/zad2.hpp
+++ include/zad2.hpp
@@ -5,5 +5,16 @@
 #include <type_traits>
 
 // tutaj klasa Penne
+class Penne : public Makaron
+{
+	double ileMaki(unsigned P) const override { return static_cast<double> (P); };
+};
 
 // tutaj definicja metody gotujMakaron
+Makaron* Makaron::gotujMakaron(const std::string& s)
+{
+	char firstChar = s.at(0);
+	char lastChar = s.at(s.length() - 1);
+	if (firstChar == lastChar) return new Tagliatelle(3.14, 0.42, 0.1);
+	else return new Penne();
+}
