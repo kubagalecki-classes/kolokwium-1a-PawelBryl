38c1f599f6122c248def5ca9ff6f212542d37757
PawelBryl <73634622+PawelBryl@users.noreply.github.com>
2021-01-14 13:39:57 +0100

    Update zad1.hpp

--- include/zad1.hpp
+++ include/zad1.hpp
@@ -3,6 +3,24 @@
 // Zad2
 // tutaj definicja klasy Makaron
 // BEZ definicji metody gotujMakaron (tylko z deklaracją)
-
+class Makaron
+{
+public:
+	virtual double ileMaki(unsigned P) const = 0;
+	static Makaron* gotujMakaron(const std::string& s);
+	virtual  ~Makaron() = default;
+};
 // Zad1
 // tutaj definicja klasy Tagliatelle
+class Tagliatelle : public Makaron
+{
+private:
+	double L;
+	double W;
+	double R;
+  static const double C;
+public:
+	Tagliatelle() : L(0.5), W(0.5), R(0.5) {}
+	Tagliatelle(double l, double w, double r) : L(l), W(w), R(r) {}
+	double ileMaki(unsigned P) const override { return P * L * W * (1. - R) * C; }
+};
